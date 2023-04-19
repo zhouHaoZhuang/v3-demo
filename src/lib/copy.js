364c91bf19bd40f17ed58daeb4ec24fa6d13714d
zhouHaoZhuang <88356815+zhouHaoZhuang@users.noreply.github.com>
2023-04-19 17:28:04 +0800

    增加工具函数

---
+++ src/lib/copy.js
@@ -0,0 +1,17 @@
+export const copyText = (function () {
+  if (navigator.clipboard) {
+    return (text) => {
+      navigator.clipboard.writeText(text)
+    }
+  } else {
+    return (text) => {
+      console.log(text)
+      const input = document.createElement('input')
+      input.setAttribute('value',text)
+      document.body.appendChild(input)
+      input.select()
+      document.execCommand('copy')
+      document.body.removeChild(input)
+    }
+  }
+})();
