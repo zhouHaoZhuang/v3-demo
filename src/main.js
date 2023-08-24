3ed3d0f0ebc1066862d4e3c8b13c5460ad846042
zhouhaozhuang <3096955582@qq.com>
2023-08-24 18:31:45 +0800

    修改引入

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import components from '@/components/index.js'
 import directives from '@/directive/index.js'
+import components from '@/ui/index.js'
 import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
 
 const app = createApp(App)
-directives(app)
+directives(app) // 注册全局指令
+components(app)  // 注册全局组件
 app.use(createPinia())
 app.use(router)
-app.use(components)
 app.mount('#app')
